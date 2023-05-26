2a2bbabc1044477d2ce44258ccaeaa6751e047b0
Furkan Simsek <simsekfurkann07@gmail.com>
2023-05-26 15:43:40 +0300

    data.tsx

--- client/src/components/post/Data.tsx
+++ client/src/components/post/Data.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import styled from "styled-components";
 import { IComment } from "../../interfaces/ISlices";
+import LoadingBox from "../LoadingBox";
 
 type DataProps = {
   loading: boolean;
@@ -8,15 +9,37 @@ type DataProps = {
   data: IComment[];
 };
 
-const Data: FC<DataProps> = ({ data, hasmore, loading }) => (
-  <DataContainer>
-    <h1>Todo</h1>
-  </DataContainer>
-);
+const Data: FC<DataProps> = ({ data, hasmore, loading }) => {
+  return (
+    <DataContainer>
+      <Content />
+      {data.map((comment) => (
+        <CommentItem />
+      ))}
+      {loading && <LoadingBox />}
+    </DataContainer>
+  );
+};
 
-const DataContainer = styled.div`
+const DataContainer = styled.ul`
   height: calc(100% - 130px - 4rem);
   overflow-y: auto;
+  .loading-box {
+    margin: 1rem 0px;
+  }
+  padding: 1rem;
 `;
+
+const Content = () => (
+  <div>
+    <h1>Content</h1>
+  </div>
+);
+
+const CommentItem = () => (
+  <div>
+    <h1>Commentitem</h1>
+  </div>
+);
 
 export default Data;
