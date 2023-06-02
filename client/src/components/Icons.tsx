63882ef38a6d1bb5c5566331872f8ebc5fd8c358
Furkan Simsek <simsekfurkann07@gmail.com>
2023-06-03 01:27:31 +0300

    i

--- client/src/components/Icons.tsx
+++ client/src/components/Icons.tsx
@@ -720,3 +720,64 @@ export const RemoveIcon2 = () => (
     ></line>
   </svg>
 );
+
+export const SettingsIcon = () => (
+  <svg
+    aria-label="Options"
+    color="rgb(245, 245, 245)"
+    fill="rgb(245, 245, 245)"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <title>Options</title>
+    <circle
+      cx="12"
+      cy="12"
+      fill="none"
+      r="8.635"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    ></circle>
+    <path
+      d="M14.232 3.656a1.269 1.269 0 0 1-.796-.66L12.93 2h-1.86l-.505.996a1.269 1.269 0 0 1-.796.66m-.001 16.688a1.269 1.269 0 0 1 .796.66l.505.996h1.862l.505-.996a1.269 1.269 0 0 1 .796-.66M3.656 9.768a1.269 1.269 0 0 1-.66.796L2 11.07v1.862l.996.505a1.269 1.269 0 0 1 .66.796m16.688-.001a1.269 1.269 0 0 1 .66-.796L22 12.93v-1.86l-.996-.505a1.269 1.269 0 0 1-.66-.796M7.678 4.522a1.269 1.269 0 0 1-1.03.096l-1.06-.348L4.27 5.587l.348 1.062a1.269 1.269 0 0 1-.096 1.03m11.8 11.799a1.269 1.269 0 0 1 1.03-.096l1.06.348 1.318-1.317-.348-1.062a1.269 1.269 0 0 1 .096-1.03m-14.956.001a1.269 1.269 0 0 1 .096 1.03l-.348 1.06 1.317 1.318 1.062-.348a1.269 1.269 0 0 1 1.03.096m11.799-11.8a1.269 1.269 0 0 1-.096-1.03l.348-1.06-1.317-1.318-1.062.348a1.269 1.269 0 0 1-1.03-.096"
+      fill="none"
+      stroke="currentColor"
+      stroke-linejoin="round"
+      stroke-width="2"
+    ></path>
+  </svg>
+);
+
+export const ArrowRight = () => (
+  <svg
+    aria-label="Next"
+    color="rgb(0, 0, 0)"
+    fill="rgb(0, 0, 0)"
+    height="16"
+    role="img"
+    viewBox="0 0 24 24"
+    width="16"
+  >
+    <title>Next</title>
+    <path d="M21 17.502a.997.997 0 0 1-.707-.293L12 8.913l-8.293 8.296a1 1 0 1 1-1.414-1.414l9-9.004a1.03 1.03 0 0 1 1.414 0l9 9.004A1 1 0 0 1 21 17.502Z"></path>
+  </svg>
+);
+
+export const ArrowLeft = () => (
+  <svg
+    aria-label="Go Back"
+    color="rgb(0, 0, 0)"
+    fill="rgb(0, 0, 0)"
+    height="16"
+    role="img"
+    viewBox="0 0 24 24"
+    width="16"
+  >
+    <title>Go Back</title>
+    <path d="M21 17.502a.997.997 0 0 1-.707-.293L12 8.913l-8.293 8.296a1 1 0 1 1-1.414-1.414l9-9.004a1.03 1.03 0 0 1 1.414 0l9 9.004A1 1 0 0 1 21 17.502Z"></path>
+  </svg>
+);
