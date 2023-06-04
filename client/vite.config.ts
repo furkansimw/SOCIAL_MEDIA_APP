f2eeffc39ae92c1cd1852ae4814de019cd789ad5
Furkan Simsek <simsekfurkann07@gmail.com>
2023-06-04 23:01:47 +0300

    e

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,15 +3,11 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import dotenv from "dotenv";
 
 export default defineConfig(({ mode }) => {
-  // .env dosyasını yükleyin
   const env = dotenv.config().parsed;
-
-  // env değişkenlerini Vite'a aktarın
   const envKeys = Object.keys(env).reduce((acc, key) => {
     acc[`import.meta.env.${key}`] = JSON.stringify(env[key]);
     return acc;
   }, {});
-
   return {
     plugins: [reactRefresh()],
     define: {
