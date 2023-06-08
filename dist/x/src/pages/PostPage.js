3f4473fce5ab4e31d9854bf2a1fef61b65c69d3c
Furkan Simsek <simsekfurkann07@gmail.com>
2023-06-08 21:10:57 +0300

    x

--- dist/x/src/pages/PostPage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const PostPage = () => {
-    return (<div>
-      <h1>Furkan</h1>
-    </div>);
-};
-exports.default = PostPage;
