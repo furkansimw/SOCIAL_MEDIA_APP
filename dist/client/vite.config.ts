a1bea86ffe37dc0dccae07a0689b2396aef6354e
furkansimw <furkanworkcool@gmail.com>
2023-06-16 01:34:01 +0300

    source map disable

--- dist/client/vite.config.ts
+++ dist/client/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { sourcemap: false },
   server: {
     proxy: {
       "/api": "http://localhost:4000",
